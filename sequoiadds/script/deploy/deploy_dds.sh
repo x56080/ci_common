2dd82649035b745f55f5386211893c5bd0b4c12f
wangwenjing <wangwenjing@sequoiadb.com>
2024-03-05 09:27:59 +0800

    dds java驱动测试构建失败

--- sequoiadds/script/deploy/deploy_dds.sh
+++ sequoiadds/script/deploy/deploy_dds.sh
@@ -113,7 +113,7 @@ else
 fi
 
 sed -i "s/\${PORT}/${port}/g" deployconfig.yml
-latest_tool_package=$(wget -qO- http://192.168.29.80:8080/view/daily_tools/job/dailybuild_clusterconfig/lastSuccessfulBuild/api/json | grep -oE 'sdb-dds-cc_v[0-9]+\.[0-9]+\.[0-9]+\.tar\.gz' | tail -n 1)
+latest_tool_package=$(wget -qO- http://192.168.29.80:8080/view/daily_tools/job/dailybuild_clusterconfig/lastSuccessfulBuild/api/json | grep -oE 'sdb-dds-cc_v[0-9]+\.[0-9]+(\.[0-9]+)?(-[a-zA-Z0-9]+)?\.tar\.gz' | tail -n 1)
 # 下载最新版本的工具包
 wget "http://192.168.29.80:8080/view/daily_tools/job/dailybuild_clusterconfig/lastSuccessfulBuild/artifact/build/$latest_tool_package"
 test $? -ne 0 && echo "download cc failed!" && exit 1
