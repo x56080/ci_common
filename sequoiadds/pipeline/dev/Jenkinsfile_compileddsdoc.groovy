bef7dc4c51e268ffc0e4180393bde8e5f3911d16
zhouhongye <zhouhongye@sequoiadb.com>
2024-03-07 15:37:03 +0000

    dds-doc 构建更新

--- sequoiadds/pipeline/dev/Jenkinsfile_compileddsdoc.groovy
+++ sequoiadds/pipeline/dev/Jenkinsfile_compileddsdoc.groovy
@@ -9,9 +9,10 @@ pipeline {
         string(name: 'git_rep', defaultValue: 'http://gitlab.sequoiadb.com/sequoiadb/dds-doc.git', description: '')
         string(name: 'branch', defaultValue: 'main', description: '')
         string(name: 'docker_rep', defaultValue: '192.168.20.106')
-        string(name: 'docker_image', defaultValue: '192.168.20.106/sequoiadb/dds-doc-builder:0.2.0', description: '')
+        string(name: 'docker_image', defaultValue: '192.168.20.106/sequoiadb/dds-doc-builder:0.3.0', description: '')
         string(name: 'docker_user', defaultValue: 'liuyuchen', description: '')
         string(name: 'docker_passwd', defaultValue: 'liuyc_2021', description: '')
+        string(name: 'build_script_arguments', defaultValue: '--pdf --pdf-name-prefix sequoiadb-dds-manual', description: 'arguments pass to build.py')
     }
 
     stages{
@@ -34,22 +35,22 @@ pipeline {
                 docker{
                     image "${params.docker_image}"
                     label 'compile_x86_dds'
-                    args "-v $WORKSPACE/dds-doc:/opt/src/dds-doc"
+                    args "--entrypoint= -v $WORKSPACE/dds-doc:/book"
                 }
             }
-        	steps{
-        		sh "python3 /usr/src/build.py /opt/src/dds-doc"
+            steps{
+        		sh "python3 /usr/src/build.py /book ${build_script_arguments}"
         	}
         }
     }
 
     post{
     	success{
-    		archiveArtifacts allowEmptyArchive: true, artifacts: 'dds-doc/site/*.pdf'
+    		archiveArtifacts allowEmptyArchive: true, artifacts: 'dds-doc/book/*.pdf'
     	}
 
     	failure {
-    		emailext body: '$DEFAULT_CONTENT', subject: '$DEFAULT_SUBJECT', to: '$DEFAULT_RECIPIENTS,liuyuchen@sequoiadb.com'
+    		emailext body: '$DEFAULT_CONTENT', subject: '$DEFAULT_SUBJECT', to: '$DEFAULT_RECIPIENTS,liuyuchen@sequoiadb.com,zhouhongye@sequoiadb.com'
     	}
     }
 }
