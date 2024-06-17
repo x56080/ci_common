fa6604db307957483dc2ed6e1db43682cdbcd9a4
yangqincheng <yangqincheng@sequoiadb.com>
2024-06-17 16:53:07 +0800

    备份恢复 agent 去除 enterprise 编译参数

--- sequoiadds/pipeline/test/Jenkinsfile_dds_backup_dailybuild.groovy
+++ sequoiadds/pipeline/test/Jenkinsfile_dds_backup_dailybuild.groovy
@@ -10,7 +10,6 @@ pipeline {
     parameters{
         string(name: 'branch', defaultValue: 'main', description: '')
         string(name: 'cc_version', defaultValue: '1.0.4', description: '')
-        booleanParam(name: 'is_enterprise', defaultValue: 'false', description: '')
         booleanParam(name: 'is_release', defaultValue: 'false', description: '')
     }
     
@@ -19,7 +18,7 @@ pipeline {
             parallel {
                 stage("compile dds-backup") {
                     steps {
-                        build job: 'compile_dds-backup_agent', parameters: [string(name: 'branch', value: "${params.branch}"),booleanParam(name: 'is_enterprise', value: "${params.is_enterprise}")], wait: true
+                        build job: 'compile_dds-backup_agent', parameters: [string(name: 'branch', value: "${params.branch}")], wait: true
                     }
                 }
                 
@@ -38,7 +37,7 @@ pipeline {
                         build job: 'test_dds_backup_driver_x86', parameters: [string(name: 'branch', value: "${params.branch}"),string(name: 'dds_version', value: "${params.dds_version}"),string(name: 'cc_version', value: "${params.cc_version}")], wait: true
                     }
                 }
-                
+
                 stage("arm test") {
                     steps {
                         build job: 'test_dds_backup_driver_arm', parameters: [string(name: 'branch', value: "${params.branch}"),string(name: 'dds_version', value: "${params.dds_version}"),string(name: 'cc_version', value: "${params.cc_version}")], wait: true
