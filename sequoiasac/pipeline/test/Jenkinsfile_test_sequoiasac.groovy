4ad2604abf9bca099ebce99bec47e50f8ed9679e
王文净 <23-wangwenjing@users.noreply.gitlab.example.com>
2024-07-17 12:40:39 +0000

    测试代码拉取，30分钟超时

--- sequoiasac/pipeline/test/Jenkinsfile_test_sequoiasac.groovy
+++ sequoiasac/pipeline/test/Jenkinsfile_test_sequoiasac.groovy
@@ -75,7 +75,7 @@ pipeline {
             steps {
                 script{
                    def branch="${params.test_branch}"
-                   checkout scmGit(branches: [[name: "${branch}"]], extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'sac-auto-test']], userRemoteConfigs: [[url: "${params.test_repository}"]])
+                   checkout scmGit(branches: [[name: "${branch}"]], extensions: [checkoutOption(30),[$class: 'RelativeTargetDirectory', relativeTargetDir: 'sac-auto-test']], userRemoteConfigs: [[url: "${params.test_repository}"]])
                    sh ". ~/.virtualenvs/env/bin/activate;cd sac; python localbuild.py --${params.testtype};deactivate"
                 }
             }
