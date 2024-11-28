e31d66f054e068e13299a6dc6080b602a068e3e2
huangzhiming <116-huangzhiming@users.noreply.gitlab.example.com>
2024-11-28 03:44:05 +0000

    Update Jenkinsfile_test_sequoiasac.groovy

--- sequoiasac/pipeline/test/Jenkinsfile_test_sequoiasac.groovy
+++ sequoiasac/pipeline/test/Jenkinsfile_test_sequoiasac.groovy
@@ -2,7 +2,8 @@ pipeline {
     agent {label 'test_sac'}
     parameters {
         string(name: 'dev_repository', defaultValue: 'http://gitlab.sequoiadb.com/sequoiadb/sac.git', description: '')
-        string(name: 'test_repository', defaultValue: 'http://gitlab.sequoiadb.com/test/sac-auto-test', description: '')
+        string(name: 'test_repository', defaultValue: 'http://gitlab.sequoiadb.com/test/sac-auto-test', description: '')        
+        string(name: 'test_branch', defaultValue: '', description: '')
         choice(name: 'host_arch', choices: ['x86_64','aarch64'], description: 'Select target architecture')
         choice(name: 'testtype', choices: ['runbase','runtest'], description: 'Select test type')
     }
