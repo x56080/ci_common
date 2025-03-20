8225cec3873de0322f42c305bb29ccf2361e81fa
Administrator <zhangsuonan@sequoiadb.com>
2025-03-20 02:29:02 +0000

    Replace Jenkinsfile_test_sequoiasac.groovy

--- sequoiasac/pipeline/test/Jenkinsfile_test_sequoiasac.groovy
+++ sequoiasac/pipeline/test/Jenkinsfile_test_sequoiasac.groovy
@@ -96,8 +96,8 @@ pipeline {
             script{
                 def htmlFile = "sac-auto-testcase/mochawesome-report/mochawesome.html"
                 def ret = sh returnStdout: true, script: "grep -o 'failures&quot;:[0-9]\\+' $htmlFile |sed 's/.*://'"
-				ret = ret.trim()
-				println "ret value: ${ret}"
+       ret = ret.trim()
+       println "ret value: ${ret}"
                 if (ret != 0 ){
                     currentBuild.result = "UNSTABLE"
                     //def src_basedir="sac-auto-testcase/testcases/story/Cypress"
